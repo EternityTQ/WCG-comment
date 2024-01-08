5539fc6106c9609ad4aa4abd7b7494486747703c
EternityTQ <144424879+EternityTQ@users.noreply.github.com>
2024-01-09 04:12:54 +0800

    rework index.js

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const Application = require('@waline/vercel');
-const cors = require('cors'); // 需要安装cors模块，使用npm install cors
 
-// 允许所有源访问，或者替换'*'以允许特定源访问
-const corsOptions = {
-  origin: '*', // 或者你的前端应用的源，如'https://eternitytq.github.io'
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 根据需要允许的方法进行调整
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-};
-
-const app = Application({
+module.exports = Application({
   plugins: [],
   async postSave(comment) {
     // do what ever you want after comment saved
   },
 });
-
-app.use(cors(corsOptions)); // 应用CORS中间件
-
-module.exports = app;
