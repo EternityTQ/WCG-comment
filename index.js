33c6640e282470978d92725bbdd80dd593324374
EternityTQ <144424879+EternityTQ@users.noreply.github.com>
2024-01-09 04:47:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const Application = require('@waline/vercel');
-const { VercelRequest, VercelResponse } = require('@vercel/node');
-const cors = require('cors');
 
-const app = Application({
+module.exports = Application({
   plugins: [],
   async postSave(comment) {
     // do what ever you want after comment saved
   },
 });
-
-const allowCors = cors({
-  origin: ['http://eternitytq.github.io'], // 你可以添加更多的源
-});
-
-module.exports = (req, res) => {
-  allowCors(req, res, () => app(req, res));
-};
